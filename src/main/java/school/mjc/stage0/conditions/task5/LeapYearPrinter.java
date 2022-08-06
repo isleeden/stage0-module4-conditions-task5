d1486e4684ade5558afe25f824f5bb04029cc047
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 19:52:59 +0500

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    System.out.println("leap");
+                    return;
+                }
+            } else {
+                System.out.println("leap");
+                return;
+           }
+        }
+        System.out.println("not leap");        
     }
 }
